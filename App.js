ae5e4b74270983e6ce04fdb64f4694acdc66b242
hamza-boujnah <143039150+hamza-boujnah@users.noreply.github.com>
2023-09-28 14:37:35 +0100

    a checkpoint for react js

---
+++ App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <button type="button" class="btn">first bankai</button>
+    
+  );
+}
+
+export default App;
